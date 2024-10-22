9be438fcf99f7c053df6c3af134e2eec68a5301d
igorssobral <igorgmz1421@gmail.com>
2024-10-21 22:54:24 -0300

    feat: add modals iteractive

--- Exercicios/Modulo03/To-Do-App/script.js
+++ Exercicios/Modulo03/To-Do-App/script.js
@@ -11,15 +11,15 @@ document.getElementById('task-form').addEventListener('submit', function (e) {
   const descriptionValue = document.getElementById('description').value;
   if (validateTask(taskValue, descriptionValue)) {
     const task = {
+      id: Date.now(),
       task: taskValue,
       taskDescription: descriptionValue,
-      status: false,
+      status: true,
     };
     console.log('🚀 ~ task:', task);
     saveTask(task);
+    closeModalNewTask();
     document.getElementById('task-form').reset();
-  } else {
-    notify('erro ao salvar tarefa');
   }
 });
 
@@ -27,13 +27,18 @@ function validateTask(task, description) {
   let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 
   if (task[0] === ' ' || task.length < 4) {
+    notify('Digite uma tarefa válida!');
     return false;
   }
   if (description[0] === ' ' || description.length < 4) {
+    notify('Digite uma descrição válida!');
     return false;
   }
 
-  if (tasks.includes(task)) {
+  const taskExists = tasks.some((t) => t.task === task);
+
+  if (taskExists) {
+    notify('Essa Tarefa já existe!');
     return false;
   }
 
@@ -45,62 +50,229 @@ function saveTask(task) {
 
   tasks.push(task);
   localStorage.setItem('tasks', JSON.stringify(tasks));
-  exibirTarefas();
+  addCard(task);
   notify('Tarefa salva com sucesso!');
+}
+
+function finishTask(task) {
+  let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+
+  const updatedTask = {
+    ...task,
+    status: false,
+  };
+  const taskIndex = tasks.findIndex((t) => t.id === task.id);
+
+  if (taskIndex !== -1) {
+    tasks[taskIndex].status = false;
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    notify('Tarefa Finalizada!');
+    exibirTarefas();
+  }
+}
+
+function updateTask(taskId) {
+  console.log('🚀 ~ updateTask ~ taskId:', taskId);
+  let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+
+  const updatedTaskName = document.getElementById('update-task').value.trim();
+  const updatedDescription = document
+    .getElementById('update-description')
+    .value.trim();
+
+  if (updatedTaskName === '' || updatedDescription === '') {
+    notify('Erro: Preencha todos os campos!');
+    return;
+  }
+
+  const taskIndex = tasks.findIndex((t) => t.id === taskId);
+  const taskt = tasks.find((t) => t.id === taskId);
+  const taskExists = tasks.some((t) => t.task === taskt.task);
+
+  if (taskExists) {
+    notify('Ja existe uma tarefa com esse nome!');
+
+    return;
+  }
+  if (taskIndex !== -1) {
+    tasks[taskIndex].task = updatedTaskName;
+    tasks[taskIndex].taskDescription = updatedDescription;
+
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+
+    closeModalEditTask();
+    notify('Tarefa Atualizada!');
+    exibirTarefas();
+  } else {
+    notify('Erro: Tarefa não encontrada!');
+  }
+}
+
+function deleteTask(task) {
+  console.log('🚀 ~ updateTask ~ task:', task);
+  let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+  console.log('🚀 ~ deleteTask ~ tasks:', tasks);
+
+  const newListTask = tasks.filter((t) => t.task !== task.task);
+
+  localStorage.setItem('tasks', JSON.stringify(newListTask));
+  exibirTarefas();
 }
 
 function exibirTarefas() {
   let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+  const tasksSorted = tasks.sort((a, b) => a.task.localeCompare(b.task));
+console.log(tasks.length)
+  const taskList = document.getElementById('task-list');
+  tasks.length < 4
+    ? taskList.classList.add('justify-center')
+    : taskList.classList.add('justify-start');
+  taskList.innerHTML = '';
 
+  tasksSorted.forEach((task, index) => {
+    addCard(task);
+  });
+}
+
+function openModalNewTask() {
+  const modal = document.getElementById('modal-new-task');
+
+  modal.classList.remove('hidden');
+  modal.classList.add('flex');
+}
+
+function closeModalNewTask() {
+  const modal = document.getElementById('modal-new-task');
+  modal.classList.remove('flex');
+  modal.classList.add('hidden');
+}
+function openModalEditTask(task) {
+  const modal = document.getElementById('modal-edit-task');
+
+  const taskInput = document.getElementById('update-task');
+  const descriptionInput = document.getElementById('update-description');
+
+  taskInput.value = task.task;
+  descriptionInput.value = task.taskDescription;
+
+  modal.classList.remove('hidden');
+  modal.classList.add('flex');
+
+  document.getElementById('task-update-form').onsubmit = function (e) {
+    e.preventDefault();
+    updateTask(task.id);
+  };
+}
+
+function closeModalEditTask() {
+  const modal = document.getElementById('modal-edit-task');
+  modal.classList.remove('flex');
+  modal.classList.add('hidden');
+}
+
+function openModalRemoveTask(task) {
+  const modal = document.getElementById('modal-remove-task');
+
+  modal.classList.remove('hidden');
+  modal.classList.add('flex');
+
+  modal.querySelector('.remove-task').addEventListener('click', function () {
+    deleteTask(task);
+    closeModalRemoveTask();
+    notify('Tarefa excluida com sucesso!');
+  });
+}
+
+function closeModalRemoveTask() {
+  const modal = document.getElementById('modal-remove-task');
+  modal.classList.remove('flex');
+  modal.classList.add('hidden');
+}
+function openModalFinishTask(task) {
+  const modal = document.getElementById('modal-finish-task');
+
+  modal.classList.remove('hidden');
+  modal.classList.add('flex');
+
+  modal.querySelector('.finish-task').addEventListener('click', function () {
+    finishTask(task);
+    closeModalFinishTask();
+  });
+}
+
+function closeModalFinishTask() {
+  const modal = document.getElementById('modal-finish-task');
+  modal.classList.remove('flex');
+  modal.classList.add('hidden');
+}
+
+function addCard(task) {
   const taskList = document.getElementById('task-list');
 
-  tasks.forEach((task, index) => {
-    const cardTask = document.createElement('div');
-    cardTask.className =
-      'class="w-60 h-80 bg-zinc-800 rounded-md p-2 flex flex-col gap-1';
+  const cardTask = document.createElement('div');
+  cardTask.className =
+    'w-[300px] lg:w-60 max-h-[350px] bg-zinc-50 rounded-xl p-4 flex flex-col gap-1 shadow-xl border-2 border-teal-900/20';
 
-    cardTask.innerHTML = `
+  cardTask.innerHTML = `
+    <h1 class="text-zinc-900">Tarefa</h1>
+    <input
+      type="text"
+      class="h-10 rounded-md text-zinc-800 pl-1  border-2 border-teal-900/10"
+      disabled
+      value="${task.task}"
+    />
+    <h1 class="text-zinc-900">Descrição</h1>
+    <textarea
+      rows="3"
+      class="w-full rounded-md text-zinc-800 pl-1  border-2 border-teal-900/10"
+      disabled
+    >${task.taskDescription}</textarea>
+    
+    <div class="space-y-3">
+      <button
+        type="button"
+        ${task.status ? '' : 'disabled'}
+        class="w-full h-9 ${
+          task.status
+            ? 'text-zinc-50 bg-teal-700'
+            : 'text-green-500 border-2 border-green-500/20'
+        } rounded-md transition duration-1000 finish-task"
+      >
+        ${
+          task.status
+            ? 'Concluir'
+            : '<span class="text-green-500 animate-pulse space-x-4"><i class="bi bi-check-all mr-1"></i>Concluído</span> '
+        }
+      </button>
+      <button
+        type="button"
+        class="w-full h-9 text-zinc-50 bg-zinc-700 rounded-md  ${
+          task.status ? 'none' : 'hidden'
+        } edit-task "
+      >
+        Editar
+      </button>
+      <button
+        type="button"
+        class="w-full h-9 text-zinc-50 bg-red-500 rounded-md delete-task"
+      >
+        Excluir
+      </button>
+    </div>
+  `;
 
-            <h1 class="text-zinc-200">Tarefa</h1>
-            <input
-              type="text"
-              class="h-8 rounded-md text-zinc-800 pl-1 bg-zinc-100"
-              disabled
-              value="${task.task}"
-            />
-            <h1 class="text-zinc-200">Descriçao</h1>
-            <textarea
-              name=""
-              id=""
-              rows="3"
-              class="w-full rounded-md text-zinc-800 pl-1 bg-zinc-100"
-              disabled
-            >${task.taskDescription}
-</textarea
-            >
-            <div class="space-y-3">
-              <button
-                type="button"
-                class="w-full h-8 text-zinc-50 bg-green-600 rounded-sm"
-              >
-                Concluir
-              </button>
-              <button
-                type="button"
-                class="w-full h-8 text-zinc-50 bg-slate-500 rounded-sm"
-              >
-                Editar
-              </button>
-              <button
-                type="button"
-                class="w-full h-8 text-zinc-50 bg-red-500 rounded-sm"
-              >
-                Excluir
-              </button>
-            </div>
-        `;
+  taskList.appendChild(cardTask);
 
-    taskList.appendChild(cardTask);
+  cardTask.querySelector('.edit-task').addEventListener('click', function () {
+    openModalEditTask(task);
+  });
+
+  cardTask.querySelector('.delete-task').addEventListener('click', function () {
+    openModalRemoveTask(task);
+  });
+
+  cardTask.querySelector('.finish-task').addEventListener('click', function () {
+    openModalFinishTask(task);
   });
 }
 
